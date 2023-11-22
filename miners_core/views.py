e07aa45b3b00a7fbcea9f1f66af30d0adf09a89c
ajh123 <ajh123@users.noreply.github.com>
2023-11-22 17:45:32 +0000

    Move into HTML file

--- miners_core/views.py
+++ miners_core/views.py
@@ -6,12 +6,5 @@ from django.http import HttpResponse
 
 def index(request):
     now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    context = {"now": now}
+    return render(request, "index.html", context)
--- miners_online_office/asgi.py
+++ miners_online_office/asgi.py
@@ -14,3 +14,4 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'miners_online_office.settings')
 
 app = get_asgi_application()
+application = app
